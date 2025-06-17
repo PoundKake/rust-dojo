132e37ff2400a184f192f44a22531d20bcf34b25
PoundKake <vance.christian@gmail.com>
2025-06-17 10:13:41 -0400

    initial commit

---
+++ grrs/src/main.rs
@@ -0,0 +1,31 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    // The pattern to look for
+    pattern: String,
+    // The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    // let pattern = std::env::args().nth(1).expect("no pattern given");
+    // let path = std::env::args().nth(2).expect("no path given");
+    //
+    // let args = Cli {
+    //     pattern,
+    //     path: std::path::PathBuf::from(path),
+    // };
+    
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    // println!("pattern: {:?}, path {:?}", args.pattern, args.path);
+}
