72861472ea0a2649550e0cb4ad01c5dba926686f
PoundKake <vance.christian@gmail.com>
2025-06-17 17:03:11 -0400

    adding more unit tests

--- grrs/tests/cli.rs
+++ grrs/tests/cli.rs
@@ -1,5 +1,7 @@
+use assert_fs::prelude::*; // Generates a fake temp file for unit test purposes.
 use assert_cmd::prelude::*; // Add methods on command
 use std::process::Command; // run programs
+use predicates::prelude::*;
 
 #[test]
 fn file_does_not_exist() -> Result<(), Box<dyn std::error::Error>> {
@@ -9,6 +11,32 @@ fn file_does_not_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicates::str::contains("could not read file"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("A test\nAnother test"));
+
+    Ok(())
+}
+
+#[test]
+fn empty_string_integration_test() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("").arg("src/main.rs");
+
+    // NOTE: this needs and erorr case to test for 
+    cmd.assert()
+        .success();
 
     Ok(())
 }
