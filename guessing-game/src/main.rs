98f3092b416039bea35ae7c78373bb75b9be7bd5
PoundKake <vance.christian@gmail.com>
2025-07-02 12:59:21 -0400

    initial commit for guessing game.
    Game code written from docs
    
    https://doc.rust-lang.org/stable/book/ch02-00-guessing-game-tutorial.html

---
+++ guessing-game/src/main.rs
@@ -0,0 +1,16 @@
+use std::io;
+
+fn main() {
+    println!("Guess the Number!");
+
+    println!("Please input yoru guess.");
+
+    let mut guess = String::new();
+    
+    io::stdin()
+        .read_lines(&mut guess)
+        .expect("Failed to read lines!")
+
+    println!("You guessed: {}", guess);
+
+}
