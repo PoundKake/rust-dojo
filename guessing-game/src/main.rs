55e17f3b18ef6490561121fd868e7a6a9cb4274f
PoundKake <vance.christian@gmail.com>
2025-07-03 12:02:43 -0400

    finished the guessing game
    This was a practical exercise from The Rust Programming Language,
    Chapter 2

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,15 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the Number!");
 
-    println!("Please input yoru guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=5);
 
-    let mut guess = String::new();
+    // println!("Your secret number is {secret_number}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read lines!");
+    loop {
+        println!("Please input yoru guess.");
 
-    println!("You guessed: {}", guess);
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read lines!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+            Ordering::Greater => println!("Too Big!"),
+        }
+    }
 }
