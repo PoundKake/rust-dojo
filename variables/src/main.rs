5e27b3d5e5d071311ae85e0de10c5855e515c7b9
PoundKake <vance.christian@gmail.com>
2025-07-03 14:56:00 -0400

    adding examples of shawoding variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -15,5 +15,24 @@ fn main() {
     const HOURS: u32 = 60;
     const SECONDS: u32 = 60;
     const THREE_HOURS_IN_SECODNS: u32 = HOURS * SECONDS * 3;
-    println!("Three hours in seconds is: {THREE_HOURS_IN_SECODNS}s")
+    println!("Three hours in seconds is: {THREE_HOURS_IN_SECODNS}s");
+
+    // Example of shadowing variables
+    let z = 5;
+    let z = z + 1;
+
+    {
+        let z = z * 2;
+        println!("The value of x in the inner scope is: {z}");
+    }
+    println!("The value of x is: {z}");
+
+    // Example of type changes between shadowing variables and mut
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("{spaces}");
+
+    // This will give compile errors
+    // let mut spaces = "    ";
+    // spaces = spaces.len();
 }
